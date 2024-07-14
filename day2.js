cfb3997893724ec18edb526989aa2e1326378dec
Abhishek Yadav <adviteeycode@gmail.com>
2024-07-14 17:19:08 +0530

    Activities Updated -1.0

---
+++ day2.js
@@ -0,0 +1,38 @@
+// Day 2: Operators
+// Tasks/Activities
+// Activity 1: Arithmetic Operations
+    // Task 1: WAP to add two numbers and log the result to the console.
+    // Task 2: WAP to subtract two numbers and log the result to the console.
+    // Task 3: WAP to multiply two numbers and log the result to the console.
+    // Task 4: WAP to divide two numbers and log the result to the console.
+    // Task 5: WAP to find the remainder when one number is divided by another and log the result to console.
+
+// Activity 2: Assignment Operators
+    // Task 6: Use the += operator to add a number to a variable and log the result to the console.
+    // Task 7: Use the -= operator to add a number to a variable and log the result to the console.
+
+// Activity 3: Comparsion Operators
+    // Task 8: WAP to compare two numbers using > and < and log the result to the console.
+    // Task 9: WAP to compare two numbers using >= and <= and log the result to the console.
+    // Task 10: WAP to compare two numbers using == and === and log the result to the console.
+
+// Activity 4: Logical Operators
+    // Task 11: WAP that uses the && operator to combine two conditions and log the result to the console.
+    // Task 12: WAP that uses the || operator to combine two conditions and log the result to the console.
+    // Task 13: WAP that uses the ! operator to combine two conditions and log the result to the console.
+
+// Activity 5: Ternary Operator
+    // WAP that uses the ternary operator to check if a number is psitive or negative and log the result to the console.
+
+// Feature Request:
+    // 1.Arithmetic Operations Script: Write a script that performs basic arithmetic operations (addition, subtraction, multiplication, division, remainder) on two numbers and logs the results.
+    // 2.Comparison and Logical Operators Script: Create a script that compares two numbers using different comparison operators and combines conditions using logical operators, logging the results.
+    // 3.Ternary Operator Script: Write a script that uses the ternary operator to determine if a number is positive or negative and logs the result.
+
+// Achievement:
+    // By the end of these activities, students will:
+        // Understand and use arithmetic operators to perform basic calculations.
+        // Use assignment operators to modify variable values.
+        // Compare values using comparison operators.
+        // Combine conditions using logical operators.
+        // Use the ternary operator for concise conditional expressions.
