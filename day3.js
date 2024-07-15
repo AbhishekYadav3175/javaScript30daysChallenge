bf534a0ef128817d03c178ebb382b962598b5924
Abhishek Yadav <adviteeycode@gmail.com>
2024-07-15 21:28:43 +0530

    Task added -1.0

---
+++ day3.js
@@ -0,0 +1,33 @@
+// Day 3: Control Structures
+    // Tasks/Activities:
+        //Activity 1: If-Else statements
+            // Task 1: WAP to check if a number is positive, negative, or zero, and log the result to the console.
+            // Task 2: WAP to check if a person is eligible to vote (age>=18) and log the result to the console.
+
+        //Activity 2: Nested If-Else statements
+            // Task 3: Write a program to find teh largest of three numbers using nested if-else statements.
+
+        //Activity 3: Switch Case
+            // Task 4: WAP that uses a switch case determine the day of the week based on a number(1-7) and log the day name to the console.
+            // Task 5: WAP that uses a switch case to assign a grade ('a','b','c','d','f') based on a score and log the grade to the console.
+
+        //Activity 4: Conditional (Ternary) Operator
+            // Task 6: WAP that uses the ternary operator to check if a number is even or odd and log the result to the console.
+
+        //Activity 5: Combining Conditions
+            // Task 7: WAP to check if a year is a leap year using multiple conditions (divisible by 4, but not 100 unless alos divisible by 400) and log the result to the console.
+
+// Feature Request:
+    // 1. Number Check Script: Write a script that checks if a number is positive, negative, or zero using if-else statements and logs the result.
+    // 2. Voting Eligibility Script: Create a script to check if a person is eligible to bote based on their age and log the result.
+    // 3. Day of the Week Script: Write a script that uses a switch case to determine the day of the week based on a number(1-7) and logs the day name.
+    // 4. Grade Assignment Script: Create a script that uses a switch case to assign a grade based on a score and logs the grade.
+    // 5. Leap Year Check Script: Write a script that checks if a year is a leap year using multiple conditions and logs the result.
+
+// Achievement:
+// By the end of these activities, students will:
+    // Implement and understand basic if-else control flow.
+    // Use nested if-else statements to handle multiple conditions.
+    // Utilize switch cases for control flow based on specific values.
+    // Apply the ternary operator for concise condition checking.
+    // Combine multiple conditions to solve more complex problems.
