19a852041a315e15c04124f54c08a89aab499b83
Abhishek Yadav <adviteeycode@gmail.com>
2024-07-17 12:20:26 +0530

    Day 4 tasks are completed - 2.0

--- day4.js
+++ day4.js
@@ -2,15 +2,38 @@
 // Tasks/Activities
     // Activity 1: For Loop
         // Task 1: WAP to print numbers from 1 to 10 using a for loop.
+        for(let i = 0; i<=10 ; ++i){
+            console.log(i)
+        }
         // Task 2: WAP to print the multiplication table of 5 using a for loop.
-
+        for(let i=0; i<=10 ; ++i){
+            console.log("5 x ",i," = ",5*i)
+        }
     // Activity 2: While Loop
         // Task 3: WAP to calculate the sun of numbers from 1 to 10 using a while loop.
+        let i = 0
+        while (i<10) {
+            console.log(++i)
+        }
         // Task 4: WAP to print numbers from 10 to 1 using a while loop.
+        let j = 10
+        while(j>0){
+            console.log(j--)
+        }
 
     // Activity 3: Do...While Loop
         // Task 5: WAP to print numbers from 1 to 5 using a do...while loop.
+        let k = 1
+        do{
+            console.log(k++)
+        }while(k<=5)
         // Task 6: WAP to calculate the factorial of a number using a do...while loop.
+        let factorial = 1
+        let num = 10
+        do{
+            factorial = factorial * num--
+        }while(num>0)
+        console.log(factorial)
 
     // Activity 4: Nested Loops
         // Task 7: WAP to print a pattern using nested for loops:
@@ -18,10 +41,29 @@
                 // * *
                 // * * *
                 // * * * *
-    
+        for(let i=1; i<=4 ; i++){
+            for(let j=1 ; j<=i ; j++){
+                process.stdout.write("* ")    // for printing without newline 
+            }
+            console.log()
+        }
     // Activity 5: Loop Control Statements
         // Task 8: WAP to print numbers from 1 to 10, but skip the number 5 using the continue statement.
+        for(let i=1; i<=10 ; i++){
+            if(i==5){
+                continue
+            }else{
+                console.log(i)
+            }
+        }
         // Task 9: WAP to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement.
+        for(let i=1 ; i<=10 ; i++){
+            if(i==7){
+                break
+            }else{
+                console.log(i)
+            }
+        }
 
 // Feature Request:
     // 1. Number Printing Script: Write a script that prints numbers from 1 to 10 using a for loop and a while loop.
@@ -37,4 +79,3 @@
         // Apply do...while loops to ensure the loop body is executed at least once.
         // Implement nested loops to solve more complex problems.
         // Use loop control statements(break and continue) to control the flow of loops.
-        
